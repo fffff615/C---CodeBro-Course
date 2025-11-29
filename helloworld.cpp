3063ff74ade2849416d59c1bb9b2bbfad08b1454
yayyah <yahyasaleh16082013@gmail.com>
2025-11-29 17:02:57 +0000

    first commit

---
+++ helloworld.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+
+int main() {
+    std::string name;
+    std::cout << "Enter your name: ";
+    std::getline(std::cin >> std::ws, name);
+
+    int age;
+    std::cout << "Enter your age: ";
+    std::cin >> age;
+
+    int year;
+    std::cout << "Enter the current year: ";
+    std::cin >> year;
+    int birthYear = year - age;
+
+    std::cout << "Hello, " << name << "!" << std::endl;
+    std::cout << "You are " << age << " years old." << std::endl;
+    std::cout << "You were born in " << birthYear << "." << std::endl;
+
+    return 0;
+}
