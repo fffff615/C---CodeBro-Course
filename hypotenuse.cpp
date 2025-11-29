1710e2d212c9e81e68f8e60895427507f1b2a42d
yayyah <yahyasaleh16082013@gmail.com>
2025-11-29 19:38:39 +0000

    Simplify hypotenuse calculation by combining power and square root operations

--- hypotenuse.cpp
+++ hypotenuse.cpp
@@ -12,9 +12,7 @@ int main() {
     std::cout << "Enter length of side b: ";
     std::cin >> b;
 
-    a = pow(a, 2);
-    b = pow(b, 2);
-    c = sqrt(a + b);
+    c = sqrt(pow(a, 2) + pow(b, 2));
     std::cout << "The length of the hypotenuse c is: " << c << std::endl;
 
     return 0;
